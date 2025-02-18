31a548a0c9ce4b3f08df1fb547b5f3cf195e2be2
Manish Aryan <arayanmanish@gmail.com>
2025-02-18 13:07:53 +0530

    updated Oprators in jav script in of Arthematic Operator

---
+++ Java-Script-Module/operator.js
@@ -0,0 +1,67 @@
+// There are  5 types of Operators
+
+
+// 1. Arthematic Operator
+
+let a = 20;
+let b = 10;
+let c = (a + b); // Addition Operator
+console.log("Additon of Two Number is => ",c);  
+
+// Subtraction operator
+let sub = (a - b) 
+console.log ("subtraction of two number is => ", sub);
+
+ // Multiplication Operator
+let mul = (a * b);
+console.log ("Mul of Two Number is => ", mul);
+
+//  Exponential operator an also calles power
+let expo = (a ** b); 
+console.log("Exponential of Two Number is => ", expo);
+
+// Division Operator
+let div = (a / b); 
+console.log("Divisin of Two Number is => ", div);
+
+//   Modules Operator
+let modules = (a % b); 
+console.log("Modules of Given Number is => ", modules);
+
+
+//   Modules Operator
+let num1 = 50;
+let num2 = 10;
+ let results = (num1 % num2);
+ console.log("mudules of Two number is => ", results);
+
+
+ // increment operator
+ console.log(num1++); // post increment
+ console.log("Here is orignal value of num1 and add post increment value =>", num1); // my output should be 51
+ console.log("pre increment value", ++num1); // pre increment value is 51 + 1=> 52 is my out put
+
+ //  Decrement Operator
+console.log(num2--); //  Post Decrement
+console .log("Post Decrement Value of my numner ===>", num2--) // Pre decrement value is 5 - 1 = 4 is output
+console.log("Pre Decrement Value of my number==>", --num2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
