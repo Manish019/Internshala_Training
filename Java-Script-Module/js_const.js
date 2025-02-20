d2fdade1e89b961d6438467dfe250158912e7438
Manish Aryan <arayanmanish@gmail.com>
2025-02-20 12:57:48 +0530

    add tdz file upload in temporal dead zone

---
+++ Java-Script-Module/js_const.js
@@ -0,0 +1,57 @@
+let a = 100;  // block scope
+
+let z; //initialize
+z = 10; // we can access later on
+
+
+
+
+const x = 50; // block scope
+const y = 40;
+// const y = 55;  we can not re-declared and re-assign  y
+// y = 80;
+
+// const p; ================> we can't access later on
+// p = 50;
+
+
+
+
+{
+
+const y = 75;
+console.log(y); // but e can access inside the block
+
+
+}
+
+ // console.log(y); // we can not access y out side of block
+
+
+ // Exapmles
+
+ const fruits = ['Banana', 'Apple', 'Mango'];
+ // we can update one more fruits in fist before Banana
+  fruits[3] = "Papaya";
+
+  console.log(fruits);
+
+  //fruits = [1, 2, 3]; // type error
+
+
+  // Example 2
+
+  const user = {firstName:'Manish', lastName:'Aryan'};
+
+// we can update one more object in after last name 
+
+user.hobby = 'Cricket'; // this is right 
+
+console.log(user);
+
+// but e can not access
+
+// user = {}; /// type error
+
+
+
---
+++ Java-Script-Module/temporal_dead_zone.js
@@ -0,0 +1,24 @@
+
+// Temporal Dead Zone (TDZ)
+// Are let and const variables Hoisted?
+
+// Yes
+
+// console.log(a); // First Declearing Value
+// // var a =  400; // After Initialization   // output Undefine
+
+// let a = 500; //   Uncaught ReferenceError:----->  Cannot access 'a' before initialization
+
+console.log(b);
+
+let a = 50;
+const c = 44;
+var x = 20;
+
+console.log(a);
+console.log(c);
+
+/// TDZ means let and const variables they stay in the TDZ till the value is initialize to them.
+
+//  We cn only access the value  till the value is given 
+// We can not access value of let and const before they are initialize
